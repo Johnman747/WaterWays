20dea40083530113d5c6f2ddf7a72cf052854963
Student Loaner <studentloaner@MacBook-Pro-4.local>
2019-11-11 10:42:51 -0600

    added all loactions router, saga, and reducer

--- server/routes/locations.router.js
+++ server/routes/locations.router.js
@@ -6,7 +6,13 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-    
+    queryText = `SELECT * FROM "locations";`;
+    pool.query(queryText).then((result)=>{
+        // console.log(result.rows);
+        res.send(result.rows);
+    }).catch((error)=>{
+        console.log(error);
+    })
 });
 
 /**
