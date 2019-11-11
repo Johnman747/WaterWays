5b7fbab25515c9a953af6738d54d72834cfc8dcc
Misky Abshir <Misky.Abshir@gmail.com>
2019-11-11 14:21:24 -0600

    created file for uploading images

---
+++ src/components/AddLocation/UploadImage.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class UploadImage extends Component{
+    
+    onchange = (event) =>{
+        let image = event.target.files;
+        let reader = new FileReader();
+    
+        reader.readAsDataURL(image[0]);
+        reader.onload = (event) =>{
+          const formData = {image: event.target.result}
+          this.props.setImage(formData);
+        }
+      }
+    render(){
+        return(
+            <div>
+            <input type="file" name="image" onChange={(event) => this.onchange(event)}/>
+            </div>
+        )
+    }
+}
+const mapStatetoProps = state =>({
+    state,
+});
+
+export default connect(mapStatetoProps)(UploadImage);
