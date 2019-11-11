dac690bf5bd24c442bf28c5f5e50824732006618
Kienan Lundstrom <keen.lundy@gmail.com>
2019-11-11 13:29:53 -0600

    started component

---
+++ src/components/AddPage/AddPage.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+class AddWaterSourcePage extends Component {
+
+
+
+
+
+
+    render(){
+        return(
+            <div className="AddWaterHeader">
+                <h2>Name the Water Source</h2>
+            </div>
+        )
+    }
+}
+
+
+
+export default AddWaterSourcePage;
