9d42d60cb7da0b4083ca8d87cc91b3ab490aa969
Student Loaner <studentloaner@MacBook-Pro-4.local>
2019-11-12 13:31:01 -0600

    delete routes for reviews and reports added plus single review reducer & route

--- server/routes/reviews.router.js
+++ server/routes/reviews.router.js
@@ -19,8 +19,19 @@ router.get('/:id', (req, res) => {
         });
 
 });
+router.get('/review/:id', (req,res) =>{
+    let queryText = `SELECT * FROM "reviews" WHERE "id"=$1;`;
+    pool.query(queryText, [req.params.id])
+    .then((result) =>{
+        console.log('server results XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX', result.rows);
+        res.send(result.rows);
+    }).catch((error)=>{
+        console.log(error);
+        res.sendStatus(500);
+    })
+})
 
-router.delete('/:id', (req,res) =>{
+router.delete('/review/:id', (req,res) =>{
     let queryText = `DELETE FROM "reviews" WHERE "id"=$1;`;
     console.log(req.params.id);
     pool.query(queryText, [req.params.id])
