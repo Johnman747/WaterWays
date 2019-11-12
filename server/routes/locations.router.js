752985a86860768d8aeecd8502cc1331848e94e2
Johnathan Moes <johnman747@gmail.com>
2019-11-12 14:33:49 -0600

    added map and drop pins

--- server/routes/locations.router.js
+++ server/routes/locations.router.js
@@ -68,7 +68,7 @@ router.post('/', (req, res) => {
         spigot,trail_access,road_access,campground_access,
         free_flowing,artesian_well,photo_primary,description)
     VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)`;
-
+    
     pool.query(queryText,values)
         .then((result) => {
         console.log(result)
