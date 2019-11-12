03fbe65274aeb811e3a1a3ded65ac5543d1f8a09
Misky Abshir <Misky.Abshir@gmail.com>
2019-11-12 11:54:42 -0600

    upload image works

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const locationRouter = require('./routes/locations.router');
+const photoRouter = require('./routes/Photos.router');
 // Body parser middleware
 app.use(bodyParser.json({limit: '1 gb'}));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/locations', locationRouter);
+app.use('/api/photo', photoRouter);
 
 // Serve static files
 app.use(express.static('build'));
